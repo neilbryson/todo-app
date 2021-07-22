bfb74d287ce4ac914be5b9c3edf174d2bc3a6b4f
Neil Bryson Cargamento <neilbrysonmc@gmail.com>
2021-07-22 12:18:56 +0800

    add vite checker plugin

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,15 @@
+import checker from 'vite-plugin-checker';
+
 /**
  * @type {import('vite').UserConfig}
  */
 const config = {
+  plugins: [
+    checker({ typescript: true }),
+  ],
   server: {
     open: true,
-  }
+  },
 };
 
 export default config;
