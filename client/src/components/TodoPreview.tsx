6bc38925b4375e1e1c8deba80e25c695fab892b2
Neil Bryson Cargamento <neilbrysonmc@gmail.com>
2021-07-22 13:38:34 +0800

    update TodoPreview layout

--- client/src/components/TodoPreview.tsx
+++ client/src/components/TodoPreview.tsx
@@ -17,8 +17,11 @@ export const TodoPreview = ({
   const { t } = useLocale();
 
   return (
-    <div className="border-2 rounded border-gray-400 flex flex-col p-4 bg-blue-200 mb-2 last:mb-0" {...other}>
-      <span className="font-medium text-xl mb-4">{data.title}</span>
+    <div
+      className="border-2 rounded border-gray-400 flex justify-between p-4 bg-blue-200 hover:bg-blue-300 mb-2 last:mb-0"
+      {...other}
+    >
+      <span className="font-medium text-xl">{data.title}</span>
       {!hideDate && (
         <span className="text-xs">
           {t('due_date')} <DateDisplay date={data.dueDate} />
