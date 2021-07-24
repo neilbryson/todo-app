3b7ec978690ef9ebe727bb7211658019038acfd4
Neil Bryson Cargamento <neilbryson@users.noreply.github.com>
2021-07-24 14:40:01 +0800

    build swagger on both Release and Debug

--- server/Startup.cs
+++ server/Startup.cs
@@ -80,18 +80,19 @@ namespace TodoServer
                         .AllowAnyHeader();
                 });
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(options =>
-                {
-                    foreach (var description in provider.ApiVersionDescriptions)
-                    {
-                        options.SwaggerEndpoint(
-                            $"/swagger/{description.GroupName}/swagger.json",
-                            description.GroupName.ToUpperInvariant()
-                        );
-                    }
-                });
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                foreach (var description in provider.ApiVersionDescriptions)
+                {
+                    options.SwaggerEndpoint(
+                        $"/swagger/{description.GroupName}/swagger.json",
+                        description.GroupName.ToUpperInvariant()
+                    );
+                }
+            });
 
             app.UseHttpsRedirection();
 
