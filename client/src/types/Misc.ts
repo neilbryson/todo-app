c7b583e1938e68d754f3acc270d62e9e689e240f
Neil Bryson Cargamento <neilbrysonmc@gmail.com>
2021-08-27 14:00:02 +0800

    add TailwindComponent type

--- client/src/types/Misc.ts
+++ client/src/types/Misc.ts
@@ -1,3 +1,8 @@
+import { HTMLAttributes, ReactElement } from 'react';
+
 export type RecursivePartial<T> = { [P in keyof T]?: RecursivePartial<T[P]> };
 export type Writable<T> = { -readonly [P in keyof T]: T[P] };
 export type RecursiveWritable<T> = { -readonly [P in keyof T]: RecursiveWritable<T[P]> };
+export type TailwindComponent<T, E extends HTMLElement, Excluded extends keyof HTMLAttributes<E> = 'className'> = (
+  props: T & Omit<HTMLAttributes<E>, Excluded>
+) => ReactElement<E>;
